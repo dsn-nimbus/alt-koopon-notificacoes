1054da5311cf317b1068229524d87282ca45b089
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-01-22 10:56:22 -0200

    utiliza objeto na storage para saber quando deve exibir a notificacao

--- src/alt-koopon-notificacoes.js
+++ src/alt-koopon-notificacoes.js
@@ -1,7 +1,38 @@
 ;(function(ng) {
   'use strict';
 
-  ng.module('alt.koopon.notificacoes', [])
+  ng.module('alt.koopon.notificacoes', ['emd.ng-xtorage'])
+      .constant('ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS', 'alt_koopon_notificacoes_msg')
+      .service('AltKooponNotificacoesManager', ['$xtorage', 'ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS', function($xtorage, ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS) {
+        this.retorna = function() {
+          return $xtorage.get(ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS);
+        }
+
+        this.atualiza = function(prop, valor) {
+          var infoStorage = this.retorna();
+
+          infoStorage[prop] = valor;
+
+          $xtorage.save(ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS, infoStorage);
+        }
+
+        this.cria = function(info) {
+          $xtorage.save(ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS, info);
+        }
+
+        this.limpa = function() {
+          $xtorage.remove(ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS);
+        }
+
+        this.temMensagemPraLer = function() {
+          var infoStorage = this.retorna() || {};
+          var qtdMsg = infoStorage.qtd;
+          var novaQtdMsg = infoStorage.qtdUltimaReq;
+          var limpaStorage = infoStorage.limpa;
+
+          return (novaQtdMsg > qtdMsg) && !limpaStorage;
+        }
+      }])
       .provider('AltKooponNotificacoesService', [function() {
           var self = this;
 
@@ -26,16 +57,42 @@
               return new AltKooponNotificacoes();
           }];
       }])
-      .controller('AltKooponNotificacoesController', ['$rootScope', '$interval', 'AltKooponNotificacoesService', function($rootScope, $interval, AltKooponNotificacoesService) {
+      .controller('AltKooponNotificacoesController', ['$rootScope', '$location', '$interval', 'AltKooponNotificacoesService', 'AltKooponNotificacoesManager', function($rootScope, $location, $interval, AltKooponNotificacoesService, AltKooponNotificacoesManager) {
           var self = this;
 
+          self.exibeNotificacao = false;
           self.qtdNotificacoes = 0;
           self._idInterval = 0;
+
           self._buscar = function() {
               AltKooponNotificacoesService
                       .buscar()
                       .then(function(info) {
+                          if ($location.path() === "/mensagens") {
+                            self.exibeNotificacao = false;
+                            return AltKooponNotificacoesManager.atualiza('limpa', true);
+                          }
+
                           self.qtdNotificacoes = info.qtd;
+
+                          if (!self.qtdNotificacoes) {
+                            self.exibeNotificacao = false;
+                            return AltKooponNotificacoesManager.cria({qtd: 0, qtdUltimaReq: 0, limpa: true});
+                          }
+
+                          if (!AltKooponNotificacoesManager.retorna()) {
+                            self.exibeNotificacao = true;
+                            return AltKooponNotificacoesManager.cria({qtd: self.qtdNotificacoes, qtdUltimaReq: 0, limpa: false});
+                          }
+
+                          AltKooponNotificacoesManager.atualiza('qtdUltimaReq', self.qtdNotificacoes);
+
+                          if (AltKooponNotificacoesManager.temMensagemPraLer()) {
+                            self.exibeNotificacao = true;
+                          }
+                          else {
+                            self.exibeNotificacao = false;
+                          }
                       });
           };
 
--- tests/alt-koopon-notificacoes_test.js
+++ tests/alt-koopon-notificacoes_test.js
@@ -1,7 +1,8 @@
 'use strict';
 
 describe('Service: NotificacoesModule', function () {
-    var _rootScope, _scope, _q, _interval, _httpBackend, AltKooponNotificacoesService;
+    var _rootScope, _scope, _q, _interval, _location, _httpBackend, _xtorage, AltKooponNotificacoesService, AltKooponNotificacoesManager,
+        ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS;
     var NOME_CONTROLLER  = 'AltKooponNotificacoesController as aknCtrl';
     var URL_BASE = '/api/123/rest/';
     var URL_COMPLETA = '/api/123/rest/mensagens/count';
@@ -17,49 +18,144 @@ describe('Service: NotificacoesModule', function () {
         _q = $injector.get('$q');
         _interval = $injector.get('$interval');
         _httpBackend = $injector.get('$httpBackend');
+        _xtorage = $injector.get('$xtorage');
+        _location = $injector.get('$location')
 
         AltKooponNotificacoesService = $injector.get('AltKooponNotificacoesService');
+        AltKooponNotificacoesManager = $injector.get('AltKooponNotificacoesManager');
+
+        ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS = $injector.get('ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS');
+
+        spyOn(_xtorage, 'save').and.callFake(angular.noop);
+        spyOn(_xtorage, 'remove').and.callFake(angular.noop);
     }));
 
-    describe('service', function() {
-        describe('criação', function() {
-            it('deve ter o service como objeto', function () {
-                expect(typeof AltKooponNotificacoesService).toBe('object');
-            });
+    describe('constantes', function() {
+      it('deve ter o valor correto para ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS', function() {
+        expect(ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS).toBe('alt_koopon_notificacoes_msg');
+      });
+    });
 
-            it('deve ter o valor correto para a constante', function () {
-                expect(AltKooponNotificacoesService.TEMPO_BUSCA).toBe(TEMPO_BUSCA);
-            });
+    describe('service', function() {
+      describe('AltKooponNotificacoesManager', function() {
+        describe('criação', function() {
+          it('deve retornar um objeto', function() {
+            expect(typeof AltKooponNotificacoesManager).toBe('object');
+          });
+        });
+
+        describe('retorna', function() {
+          it('deve chamar o service com o método e parâmetros corretos', function() {
+            spyOn(_xtorage, 'get').and.callFake(angular.noop);
+
+            AltKooponNotificacoesManager.retorna();
+
+            expect(_xtorage.get).toHaveBeenCalledWith(ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS);
+          });
+        })
+
+        describe('atualiza', function() {
+          it('deve chamar o método com os parâmetros corretos', function() {
+            var valorStorageInicial = {a: 1, b: 2, c: 3};
+            var valorStorageModificado = {a: 999, b: 2, c: 3};
+
+            spyOn(AltKooponNotificacoesManager, 'retorna').and.returnValue(valorStorageInicial);
+
+            AltKooponNotificacoesManager.atualiza('a', 999);
+
+            expect(_xtorage.save).toHaveBeenCalledWith(ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS, valorStorageModificado);
+          });
+        });
+
+        describe('cria', function() {
+          it('deve chamar o método com os parâmetros corretos', function() {
+            AltKooponNotificacoesManager.cria({a: 1});
+
+            expect(_xtorage.save).toHaveBeenCalledWith(ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS, {a: 1});
+          });
+        });
+
+        describe('limpa', function() {
+          it('deve chamar o service com o método e parâmetros corretos', function() {
+            AltKooponNotificacoesManager.limpa();
+
+            expect(_xtorage.remove).toHaveBeenCalledWith(ALT_KOOPON_CHAVE_NOTIFICACOES_MENSAGENS);
+          });
+        })
+
+        describe('temMensagemPraLer', function() {
+          it('deve retornar false - nada na storage', function() {
+            spyOn(AltKooponNotificacoesManager, 'retorna').and.returnValue(undefined);
+
+            expect(AltKooponNotificacoesManager.temMensagemPraLer()).toBe(false);
+          });
+
+          it('deve retornar false - mesma quantidade de mensagens', function() {
+            spyOn(AltKooponNotificacoesManager, 'retorna').and.returnValue({qtd: 1, qtdUltimaReq: 1});
+
+            expect(AltKooponNotificacoesManager.temMensagemPraLer()).toBe(false);
+          });
+
+          it('deve retornar false - tem mensagem pra ler, mas limpa está setado como true', function() {
+            spyOn(AltKooponNotificacoesManager, 'retorna').and.returnValue({qtd: 1, qtdUltimaReq: 1, limpa: true});
+
+            expect(AltKooponNotificacoesManager.temMensagemPraLer()).toBe(false);
+          });
+
+          it('deve retornar true - tem mensagens pra ler', function() {
+            spyOn(AltKooponNotificacoesManager, 'retorna').and.returnValue({qtd: 1, qtdUltimaReq: 2});
+
+            expect(AltKooponNotificacoesManager.temMensagemPraLer()).toBe(true);
+          });
+
+          it('deve retornar true - tem mensagens pra ler', function() {
+            spyOn(AltKooponNotificacoesManager, 'retorna').and.returnValue({qtd: 0, qtdUltimaReq: 2, limpa: false});
+
+            expect(AltKooponNotificacoesManager.temMensagemPraLer()).toBe(true);
+          });
+        });
+      });
+
+      describe('AltKooponNotificacoesService', function() {
+        describe('criação', function() {
+          it('deve ter o service como objeto', function () {
+            expect(typeof AltKooponNotificacoesService).toBe('object');
+          });
+
+          it('deve ter o valor correto para a constante', function () {
+            expect(AltKooponNotificacoesService.TEMPO_BUSCA).toBe(TEMPO_BUSCA);
+          });
         });
 
         describe('buscar', function() {
-            it('deve tentar buscar a quantidade de mensagem, mas servidor retorna erro', function() {
-                _httpBackend.expectGET(URL_COMPLETA).respond(400, {erro: 1});
+          it('deve tentar buscar a quantidade de mensagem, mas servidor retorna erro', function() {
+            _httpBackend.expectGET(URL_COMPLETA).respond(400, {erro: 1});
 
-                AltKooponNotificacoesService
-                    .buscar()
-                    .then(function() {
-                      expect(true).toBeFalsy();
-                    })
-                    .catch(function(erro) {
-                        expect(erro).toEqual({erro: 1});
-                    });
-
-                _httpBackend.flush();
+            AltKooponNotificacoesService
+            .buscar()
+            .then(function() {
+              expect(true).toBeFalsy();
+            })
+            .catch(function(erro) {
+              expect(erro).toEqual({erro: 1});
             });
 
-            it('deve tentar buscar a quantidade de mensagem, mas servidor retorna erro', function() {
-                _httpBackend.expectGET(URL_COMPLETA).respond(200, {qtd: 0});
+            _httpBackend.flush();
+          });
 
-                AltKooponNotificacoesService
-                    .buscar()
-                    .then(function(info) {
-                        expect(info).toEqual({qtd: 0});
-                    });
+          it('deve tentar buscar a quantidade de mensagem, mas servidor retorna erro', function() {
+            _httpBackend.expectGET(URL_COMPLETA).respond(200, {qtd: 0});
 
-                _httpBackend.flush();
+            AltKooponNotificacoesService
+            .buscar()
+            .then(function(info) {
+              expect(info).toEqual({qtd: 0});
             });
+
+            _httpBackend.flush();
+          });
         });
+      });
     });
 
     describe('controller', function() {
@@ -72,6 +168,12 @@ describe('Service: NotificacoesModule', function () {
                 $controller(NOME_CONTROLLER, {$scope: _scope});
 
                 expect(_scope.aknCtrl.qtdNotificacoes).toBe(0);
+            }));
+
+            it('deve ter a exibeNotificacao como false', inject(function($controller) {
+                $controller(NOME_CONTROLLER, {$scope: _scope});
+
+                expect(_scope.aknCtrl.exibeNotificacao).toBe(false);
             }));
         });
 
@@ -86,6 +188,26 @@ describe('Service: NotificacoesModule', function () {
                 _interval.flush(TEMPO_BUSCA - 1);
 
                 expect(AltKooponNotificacoesService.buscar).toHaveBeenCalled();
+            }))
+
+            it('deve registrar o evento de $locationChangeSuccess', inject(function($controller) {
+                spyOn(AltKooponNotificacoesService, 'buscar').and.callFake(function() {
+                    return _q.when({qtd: 999});
+                });
+
+                spyOn(_rootScope, '$on').and.callThrough();
+
+                $controller(NOME_CONTROLLER, {$scope: _scope});
+
+                _rootScope.$digest();
+
+                _rootScope.$broadcast('$locationChangeSuccess');
+
+                expect(AltKooponNotificacoesService.buscar).toHaveBeenCalled();
+
+                _rootScope.$digest();
+
+                expect(_scope.aknCtrl.qtdNotificacoes).toBe(999);
             }))
 
             it('deve chamar a busca corretamente, tempo passou - interval', inject(function($controller) {
@@ -114,24 +236,110 @@ describe('Service: NotificacoesModule', function () {
                 expect(_scope.aknCtrl.qtdNotificacoes).toEqual(999);
             }))
 
-            it('deve registrar o evento de $locationChangeSuccess', inject(function($controller) {
+            it('não deve criar o objeto de notificações na storage - quantidade é zero', inject(function($controller) {
+                spyOn(AltKooponNotificacoesService, 'buscar').and.callFake(function() {
+                    return _q.when({qtd: 0});
+                });
+
+                spyOn(AltKooponNotificacoesManager, 'retorna').and.returnValue(undefined);
+                spyOn(AltKooponNotificacoesManager, 'cria').and.returnValue(undefined);
+                spyOn(AltKooponNotificacoesManager, 'atualiza').and.returnValue(undefined);
+
+                $controller(NOME_CONTROLLER, {$scope: _scope});
+
+                _interval.flush(TEMPO_BUSCA + 1);
+                _rootScope.$digest();
+
+                expect(AltKooponNotificacoesService.buscar).toHaveBeenCalled();
+                expect(_scope.aknCtrl.qtdNotificacoes).toEqual(0);
+                expect(_scope.aknCtrl.exibeNotificacao).toBe(false);
+                expect(AltKooponNotificacoesManager.cria).toHaveBeenCalledWith({qtd: 0, qtdUltimaReq: 0, limpa: true});
+                expect(AltKooponNotificacoesManager.atualiza).not.toHaveBeenCalled();
+            }))
+
+            it('deve criar o objeto de notificações na storage - nada existia lá', inject(function($controller) {
+                spyOn(AltKooponNotificacoesService, 'buscar').and.callFake(function() {
+                    return _q.when({qtd: 1});
+                });
+
+                spyOn(AltKooponNotificacoesManager, 'retorna').and.returnValue(undefined);
+                spyOn(AltKooponNotificacoesManager, 'atualiza').and.returnValue(undefined);
+                spyOn(AltKooponNotificacoesManager, 'cria').and.returnValue(undefined);
+
+                $controller(NOME_CONTROLLER, {$scope: _scope});
+
+                _interval.flush(TEMPO_BUSCA + 1);
+                _rootScope.$digest();
+
+                expect(AltKooponNotificacoesService.buscar).toHaveBeenCalled();
+                expect(_scope.aknCtrl.qtdNotificacoes).toEqual(1);
+                expect(_scope.aknCtrl.exibeNotificacao).toBe(true);
+                expect(AltKooponNotificacoesManager.cria).toHaveBeenCalledWith({qtd: 1, qtdUltimaReq: 0, limpa: false});
+                expect(AltKooponNotificacoesManager.atualiza).not.toHaveBeenCalled();
+            }))
+
+            it('deve atualizar o objeto de notificações na storage - mas não tem mensagens pra ler', inject(function($controller) {
+                spyOn(AltKooponNotificacoesService, 'buscar').and.callFake(function() {
+                    return _q.when({qtd: 1});
+                });
+
+                spyOn(AltKooponNotificacoesManager, 'retorna').and.returnValue({});
+                spyOn(AltKooponNotificacoesManager, 'atualiza').and.returnValue(undefined);
+                spyOn(AltKooponNotificacoesManager, 'cria').and.returnValue(undefined);
+                spyOn(AltKooponNotificacoesManager, 'temMensagemPraLer').and.returnValue(false);
+
+                $controller(NOME_CONTROLLER, {$scope: _scope});
+
+                _interval.flush(TEMPO_BUSCA + 1);
+                _rootScope.$digest();
+
+                expect(AltKooponNotificacoesService.buscar).toHaveBeenCalled();
+                expect(_scope.aknCtrl.qtdNotificacoes).toEqual(1);
+                expect(AltKooponNotificacoesManager.cria).not.toHaveBeenCalled();
+                expect(AltKooponNotificacoesManager.atualiza).toHaveBeenCalledWith('qtdUltimaReq', 1);
+                expect(_scope.aknCtrl.exibeNotificacao).toBe(false);
+            }))
+
+            it('deve atualizar o objeto de notificações na storage - e tem mensagens pra ler', inject(function($controller) {
+                spyOn(AltKooponNotificacoesService, 'buscar').and.callFake(function() {
+                    return _q.when({qtd: 1});
+                });
+
+                spyOn(AltKooponNotificacoesManager, 'retorna').and.returnValue({});
+                spyOn(AltKooponNotificacoesManager, 'atualiza').and.returnValue(undefined);
+                spyOn(AltKooponNotificacoesManager, 'cria').and.returnValue(undefined);
+                spyOn(AltKooponNotificacoesManager, 'temMensagemPraLer').and.returnValue(true);
+
+                $controller(NOME_CONTROLLER, {$scope: _scope});
+
+                _interval.flush(TEMPO_BUSCA + 1);
+                _rootScope.$digest();
+
+                expect(AltKooponNotificacoesService.buscar).toHaveBeenCalled();
+                expect(_scope.aknCtrl.qtdNotificacoes).toEqual(1);
+                expect(AltKooponNotificacoesManager.cria).not.toHaveBeenCalled();
+                expect(AltKooponNotificacoesManager.atualiza).toHaveBeenCalledWith('qtdUltimaReq', 1);
+                expect(_scope.aknCtrl.exibeNotificacao).toBe(true);
+            }))
+
+            it('não deve chamar preencher as notificacoes com o retorno do service - location é /mensagens', inject(function($controller) {
                 spyOn(AltKooponNotificacoesService, 'buscar').and.callFake(function() {
                     return _q.when({qtd: 999});
                 });
 
-                spyOn(_rootScope, '$on').and.callThrough();
+                spyOn(AltKooponNotificacoesManager, 'atualiza').and.callFake(angular.noop);
+
+                spyOn(_location, 'path').and.returnValue('/mensagens');
 
                 $controller(NOME_CONTROLLER, {$scope: _scope});
 
+                _interval.flush(TEMPO_BUSCA + 1);
                 _rootScope.$digest();
 
-                _rootScope.$broadcast('$locationChangeSuccess');
-
+                expect(_scope.aknCtrl.qtdNotificacoes).toEqual(0);
+                expect(_scope.aknCtrl.exibeNotificacao).toBe(false);
                 expect(AltKooponNotificacoesService.buscar).toHaveBeenCalled();
-
-                _rootScope.$digest();
-
-                expect(_scope.aknCtrl.qtdNotificacoes).toBe(999);
+                expect(AltKooponNotificacoesManager.atualiza).toHaveBeenCalledWith('limpa', true);
             }))
         })
     });
